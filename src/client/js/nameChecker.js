463f29ac7b703f1f928f2c882b22690e9b211465
BasmaElsisy1 <88114356+BasmaElsisy1@users.noreply.github.com>
2021-09-04 14:57:12 +0200

    Add files via upload

---
+++ src/client/js/nameChecker.js
@@ -0,0 +1,11 @@
+ function checkForName(url) {
+    var site = url.match(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/g);
+    if(site == null){
+    alert("Please add a valid URL");
+        return false;
+        
+     } else {
+        return true;
+  }}
+  
+export { checkForName }
